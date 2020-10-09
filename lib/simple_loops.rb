a49aaf424dae2f100e3dd204967d6f23c837de5a
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-09 04:50:56 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,3 +6,10 @@ def loop_message_five_times(message)
   counter+=1
 end
 end
+def loop_message_n_times(message,number)
+  counter=0
+  while counter<number do
+    puts message
+    counter+=1
+  end
+  end
